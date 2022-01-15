91ff4ec5e40f578422286abf06ad943ba58ed05a
20CS064 <97778067+20CS064@users.noreply.github.com>
2022-01-15 16:34:06 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,4 @@
+#github repository link: https://github.com/20CS064/Python-practical2
 print("20CS064 AYUSHI PRAJAPATI")
 #A:- Write a Python script to check whether a given key already exists in a dictionary.
 d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
